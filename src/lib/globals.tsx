932c69dd9633a5a79889858bb752b5cba8ffaaa6
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 15:58:45 -0700

    expanded globals to list projects and socials

--- src/lib/globals.tsx
+++ src/lib/globals.tsx
@@ -1,3 +1,5 @@
+import { FaTwitter as TwitterIcon, FaGithub as GitHubIcon, FaLinkedin as LinkedInIcon } from 'react-icons/fa'
+
 export const NAV_ITEMS = [
     {
         name: 'Home',
@@ -14,9 +16,62 @@ export const NAV_ITEMS = [
     {
         name: 'Newsletter',
         url: '/newsletter'
-    },
-    {
-        name: 'Contact',
-        url: '/contact'
     }
 ]
+
+export const SOCIAL_LINKS = [
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/SeanOliver',
+        icon: <TwitterIcon />
+    },
+    {
+        name: 'GitHub',
+        url: 'https://github.com/SeanOliver',
+        icon: <GitHubIcon />
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/theseanoliver/',
+        icon: <LinkedInIcon />
+    }
+]
+
+export const PROJECTS = [
+    {
+        name: 'SeanOliver.dev',
+        url: 'https://seanoliver.dev',
+        description: 'My personal website built with Next.js and Tailwind CSS.',
+        image: '/images/projects/seanoliver.dev.png',
+        tags: ['Next.js', 'Tailwind CSS', 'TypeScript']
+    },
+    {
+        name: 'Sharebnb',
+        url: 'https://sharebnb.vercel.app',
+        description: 'A clone of Airbnb for sharing remarkable outdoor spaces. Express backend with Next.js and React on the frontend.',
+        image: '/images/projects/sharebnb.png',
+        tags: ['Next.js', 'React', 'Express', 'TypeScript']
+    },
+    {
+        name: 'Warbler',
+        url: 'https://warbler.vercel.app',
+        description: 'A Twitter clone built with React and Firebase.',
+        image: '/images/projects/warbler.png',
+        tags: ['React', 'Firebase']
+    },
+    {
+        name: 'Jobly',
+        url: 'https://jobly.vercel.app',
+        description: 'A job board built with React and Express.',
+        image: '/images/projects/jobly.png',
+        tags: ['React', 'Express']
+    },
+    {
+        name: 'TheraGPT',
+        url: 'https://theragpt.ai',
+        description: 'A web app using OpenAI\'s GPT-3 to help people reframe negative thoughts.',
+        image: '/images/projects/theragpt.png',
+        tags: ['React', 'Express', 'OpenAI']
+    },
+]
+
