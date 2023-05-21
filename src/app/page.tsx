51a3905576a56293a15638bef10f75551e10ed4e
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 13:50:39 -0700

    refactored title and subtitle components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
-import Title from '../app/Title';
-import Subtitle from '../app/Subtitle';
-import { ProfilePic } from './components/images';
-import { metadata } from '../app/layout';
-import Image from 'next/image';
+import Title from '@/components/Title';
+import { ProfilePic } from '@/components/images';
+import { metadata } from '@/app/layout';
 
 export default function Home(): JSX.Element {
 	return (
-		<div className='Home flex flex-col items-center justify-center w-full h-screen'>
-			<div className='Home-image mb-5'><ProfilePic /></div>
+		<>
+			<div className='Home-image mb-5'>
+				<ProfilePic />
+			</div>
 			<Title title={metadata.title} />
-			<Subtitle description={metadata.description} />
-		</div>
+			<Title
+				title={metadata.description}
+				titleSize='text-2xl'
+			/>
+		</>
 	);
 }
