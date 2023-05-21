93b7c1a19a833721b73d86144016e3a0fcf14eb0
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 13:52:25 -0700

    moved nav and footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+	return (
+        <div className='Footer flex flex-row items-center justify-center w-full h-16'>
+            <div className='Footer-item rounded-full'>
+                Created by Sean Oliver
+            </div>
+        </div>
+	);
+}
---
+++ src/components/SiteNav.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { NAV_ITEMS } from '@/lib/globals';
+import { useState } from 'react';
+
+export default function SiteNav(): JSX.Element {
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<div
+			className={`
+                SiteNav
+                flex flex-row items-center justify-around
+                w-full h-24
+                container
+                sticky
+            `}>
+			<div className='SiteNav-logo'>
+				<img
+					className={`w-16 h-16`}
+					src='/favicon.ico'
+					alt='Sean Oliver'
+				/>
+			</div>
+			<button
+				className='sm:hidden block relative'
+				onClick={() => setIsOpen(!isOpen)}>
+				<div
+					className={`w-6 h-0.5 bg-slate-600 mb-1.5 transition duration-500 ease-in-out ${
+						isOpen ? 'transform rotate-45 translate-y-2' : ''
+					}`}></div>
+				<div
+					className={`w-6 h-0.5 bg-slate-600 mb-1.5 transition duration-500 ease-in-out ${
+						isOpen ? 'opacity-0' : ''
+					}`}></div>
+				<div
+					className={`w-6 h-0.5 bg-slate-600 transition duration-500 ease-in-out ${
+						isOpen ? 'transform -rotate-45 -translate-y-2' : ''
+					}`}></div>
+			</button>
+			<nav
+				className={`
+                    sm:flex sm:justify-end flex-col
+                    sm:flex-row w-full absolute
+                    top-full left-0 bg-slate-200 dark:bg-slate-800
+                    sm:static sm:bg-transparent ${
+					isOpen ? 'block' : 'hidden'
+				}`}>
+				{NAV_ITEMS.map((item, index) => (
+					<div
+						className={`
+                            SiteNav-item
+                            flex items-center
+                            p-2
+                        `}
+						key={`${item.name}-${index}`}>
+						<a
+							className={`
+                                transition duration-500 ease-in-out
+                                rounded-lg px-3 py-2
+                                text-slate-600
+                                dark:text-slate-300
+                                font-medium
+                                hover:bg-slate-100
+                                dark:hover:bg-slate-900
+                                hover:text-slate-900
+                                dark:hover:text-slate-100
+                                block w-full text-center
+                            `}
+                            href={`${item.url}`}>
+							{item.name}
+						</a>
+					</div>
+				))}
+			</nav>
+		</div>
+	);
+}
