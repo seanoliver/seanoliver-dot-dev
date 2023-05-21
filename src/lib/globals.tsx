f88361e67a07aadaea2c7c9168efe51f75a5fab9
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 13:52:09 -0700

    moved nav items to globals

---
+++ src/lib/globals.tsx
@@ -0,0 +1,22 @@
+export const NAV_ITEMS = [
+    {
+        name: 'Home',
+        url: '/'
+    },
+    {
+        name: 'About',
+        url: '/about'
+    },
+    {
+        name: 'Projects',
+        url: '/projects'
+    },
+    {
+        name: 'Newsletter',
+        url: '/newsletter'
+    },
+    {
+        name: 'Contact',
+        url: '/contact'
+    }
+]
