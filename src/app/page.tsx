718284e9c288f48cf542294b1e54e8623e004e6b
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 22:10:14 -0700

    home refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import Title from '@/components/Title';
-import { ProfilePic } from '@/components/images';
-import { metadata } from '@/app/layout';
-import ProjectCard from './projects/ProjectCard';
-import Projects from './projects/Projects';
+import Projects from './projects/page';
 import About from './about/page';
+import Experiences from './experiences/page';
 
 export default function Home(): JSX.Element {
 	return (
 		<div className='Home mb-5 flex flex-col gap-8'>
 			<About />
 			<Projects />
+			<Experiences />
 		</div>
 	);
 }
