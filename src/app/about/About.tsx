980eee8fe7733c52cd82ce6c9a232d5c09cbfe68
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 18:56:38 -0700

    rearranging files

---
+++ src/app/about/About.tsx
@@ -0,0 +1,28 @@
+import { ProfilePic } from '@/components/images';
+import Title from '@/components/Title';
+import { metadata } from '@/app/layout';
+
+import { SOCIAL_LINKS } from '@/lib/globals';
+
+export default function About(): JSX.Element {
+	return (
+		<div className='About flex justify-center align-middle self-center'>
+			<div className='About-body text-left md:w-1/2'>
+				<Title title={metadata.title} />
+				<p className={`mt-4`}>{metadata.longDescription}</p>
+				<div className='About-social-icons flex flex-row justify-center'>
+				{SOCIAL_LINKS.map((link, index) => (
+					<a
+						key={`${link.name}-${index}`}
+						className={`About-social-link text-2xl m-5`}
+						href={link.url}
+						target='_blank'
+						rel='noopener noreferrer'>
+						{link.icon}
+					</a>
+				))}
+			</div>
+			</div>
+		</div>
+	);
+}
