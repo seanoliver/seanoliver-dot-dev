be861c536da02f2d832cdc7b325efe8d3352eca0
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 18:58:39 -0700

    fade in down animations on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,21 @@ module.exports = {
       fontFamily: {
         sans: ['var(--font-jetbrains-mono)', 'sans-serif'],
       },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out'
+      },
     },
   },
   plugins: [],
