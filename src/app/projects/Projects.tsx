ea61f8cb693f7222322af7a18a4f369643774338
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 18:57:05 -0700

    project list styling

--- src/app/projects/Projects.tsx
+++ src/app/projects/Projects.tsx
@@ -3,13 +3,13 @@ import ProjectCard from "./ProjectCard";
 
 export default function Projects(): JSX.Element {
     return (
-        <>
+        <div className="flex flex-col items-center justify-center w-full h-full">
             {PROJECTS.map((project, index) => (
                 <div className={`mb-4`} key={`${project.name}-${index}`}>
                     <ProjectCard project={project} />
                 </div>
             ))}
 
-        </>
+        </div>
     );
 }
