3ef1cdf416f22fae8dc6606434db78abe8961a68
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 22:08:47 -0700

    init experiences page

---
+++ src/app/experiences/page.tsx
@@ -0,0 +1,11 @@
+import Title from "@/components/Title";
+import { EXPERIENCES } from "@/lib/globals";
+
+export default function Experiences(): JSX.Element {
+    return (
+        <div className='Experiences flex flex-col items-center justify-center w-full h-full'>
+            <Title title='Experiences' titleSize='medium' />
+
+        </div>
+    )
+}
