abd288d25da7409b3337197b02b4cf52ad23a9aa
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 22:09:36 -0700

    refactored title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,16 +3,26 @@
 /** Title component */
 export default function Title({
 	title,
-	titleSize = 'text-4xl',
+	titleSize = 'large',
 }: {
 	title: string;
-	titleSize?: 'text-4xl' | 'text-2xl' | 'text-xl' | 'text-lg' | 'text-base';
+	titleSize?: 'large' | 'medium' | 'small';
 }) {
+
+	const titleStyles = {
+		'large': 'text-4xl font-extrabold',
+		'medium': 'text-3xl font-bold',
+		'small': 'text-2xl font-semibold',
+	}
 	return (
 		<>
 			<h1
 				className={`
-				${titleSize} font-extrabold text-center py-2`}>
+				${titleStyles[titleSize]} font-extrabold text-center p-2 mb-4
+				bg-clip-text text-transparent
+				bg-gradient-to-r from-zinc-600 to-slate-600 text-white
+				dark:bg-gradient-to-r dark:from-stone-300 dark:to-slate-300 text-white
+				px-2`}>
 				{title}
 			</h1>
 		</>
