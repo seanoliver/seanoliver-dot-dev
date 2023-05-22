21ecd401c48e6e35c0cea39f633db9233768adda
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 22:09:07 -0700

    updated global variables

--- src/lib/globals.tsx
+++ src/lib/globals.tsx
@@ -1,4 +1,5 @@
 import { FaTwitter as TwitterIcon, FaGithub as GitHubIcon, FaLinkedin as LinkedInIcon } from 'react-icons/fa'
+import { Project } from '@/lib/types'
 
 export const NAV_ITEMS = [
     {
@@ -7,11 +8,11 @@ export const NAV_ITEMS = [
     },
     {
         name: 'About',
-        url: '/about'
+        url: '#about'
     },
     {
         name: 'Projects',
-        url: '/projects'
+        url: '#projects'
     },
     {
         name: 'Newsletter',
@@ -37,41 +38,85 @@ export const SOCIAL_LINKS = [
     }
 ]
 
-export const PROJECTS = [
+export const PROJECTS: Project[] = [
     {
         name: 'SeanOliver.dev',
         url: 'https://seanoliver.dev',
         description: 'My personal website built with Next.js and Tailwind CSS.',
         image: '/images/projects/seanoliver.dev.png',
-        tags: ['Next.js', 'Tailwind CSS', 'TypeScript']
+        tags: ['Next.js', 'React', 'Tailwind CSS', 'TypeScript']
     },
     {
         name: 'Sharebnb',
         url: 'https://sharebnb.vercel.app',
         description: 'A clone of Airbnb for sharing remarkable outdoor spaces. Express backend with Next.js and React on the frontend.',
         image: '/images/projects/sharebnb.png',
-        tags: ['Next.js', 'React', 'Express', 'TypeScript']
+        tags: ['Next.js', 'React', 'Express', 'JavaScript', 'AWS', 'PostgreSQL']
     },
     {
         name: 'Warbler',
         url: 'https://warbler.vercel.app',
-        description: 'A Twitter clone built with React and Firebase.',
+        description: 'A Twitter clone built with Flask.',
         image: '/images/projects/warbler.png',
-        tags: ['React', 'Firebase']
+        tags: ['Flask', 'PostgreSQL', 'JavaScript']
     },
     {
         name: 'Jobly',
         url: 'https://jobly.vercel.app',
         description: 'A job board built with React and Express.',
         image: '/images/projects/jobly.png',
-        tags: ['React', 'Express']
+        tags: ['React', 'Express', 'JavaScript', 'PostgreSQL']
     },
     {
-        name: 'TheraGPT',
+        name: 'TheraGPT v1',
         url: 'https://theragpt.ai',
         description: 'A web app using OpenAI\'s GPT-3 to help people reframe negative thoughts.',
         image: '/images/projects/theragpt.png',
-        tags: ['React', 'Express', 'OpenAI']
+        tags: ['Flask', 'OpenAI', 'JavaScript']
     },
 ]
 
+export const EXPERIENCES = [
+    {
+        title: 'Senior Product Marketing Manager',
+        company: 'Block, Inc.',
+        logo: '/images/companies/block.png',
+        date: '2021 - Present',
+        description: 'Working on the Amazon Appstore team.'
+    },
+    {
+        title: 'Director of Marketing',
+        company: 'Crew',
+        logo: '/images/companies/crew.png',
+        date: '2019 - 2021',
+        description: 'Worked on the Dealertrack DMS team.'
+    },
+    {
+        title: 'Senior Manager, Product Marketing',
+        company: 'Lyft',
+        logo: '/images/companies/lyft.png',
+        date: '2018 - 2019',
+        description: 'Worked on the Dealertrack DMS team.'
+    },
+    {
+        title: 'Senior Product Marketing Manager',
+        company: 'Optimizely',
+        logo: '/images/companies/optimizely.png',
+        date: '2017 - 2018',
+        description: 'Worked on the Dealertrack DMS team.'
+    },
+    {
+        title: 'Product Marketing Manager',
+        company: 'LinkedIn',
+        logo: '/images/companies/linkedin.png',
+        date: '2016 - 2017',
+        description: 'Worked on the Dealertrack DMS team.'
+    },
+    {
+        title: 'Product Manager',
+        company: 'Microsoft',
+        logo: '/images/companies/microsoft.png',
+        date: '2009 - 2012',
+        description: 'Worked on the Dealertrack DMS team.'
+    },
+]
