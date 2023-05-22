be50fd1cb674aaeac90d2aaf2ab781aabe3a1a0c
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-21 18:58:21 -0700

    base font switch to inter; start integrating split layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,60 +1,71 @@
 import './globals.css';
 import clsx from 'clsx';
 import SiteNav from '@/components/SiteNav';
+import NewSiteNav from '@/components/NewSiteNav';
 import Footer from '@/components/Footer';
 
-import localFont from 'next/font/local';
+/** Inter Font */
 
-const jetBrainsMono = localFont({
-	src: '../../public/fonts/JetBrainsMono-Regular.woff2',
-	variable: '--font-jetbrains-mono',
+import { Inter } from 'next/font/google';
+const inter = Inter({
+	subsets: ['latin'],
 	display: 'swap',
 });
 
-// const inter = Inter({ subsets: ['latin'] })
+/** Jet Brains Mono Font */
+
+// import localFont from 'next/font/local';
+
+// const jetBrainsMono = localFont({
+// 	src: '../../public/fonts/JetBrainsMono-Regular.woff2',
+// 	variable: '--font-jetbrains-mono',
+// 	display: 'swap',
+// });
 
 // Metadata for the home page
 export const metadata = {
-  title: 'Sean Oliver',
-  description: 'Software Engineer in San Francisco, CA',
-  longDescription: `After 13 years in the tech industry, I've seen it all - the ups, the
-  downs, and everything in between. I started as a marketer, understanding
-  what people want, what they need, and how to tell them we have it. Now, I
-  build it. I live in San Francisco. I've got a family – a wife and two
-  kids. When I'm not coding, we play tennis or travel. I read a lot, mostly
-  through audiobooks. I'm interested in how we organize our knowledge, how
-  AI is changing our world, and I love good gadgets. Professionally, I code
-  in JavaScript/TypeScript and Python. React, Flask, Express, PostgreSQL,
-  and Node.js are my tools. I use them to build things that make sense.
-  Things that are needed. Things that work. Thanks for visiting. Have a look
-  around, reach out if something catches your eye. Let's build something
-  great together.`,
-}
+	title: 'Sean Oliver',
+	description: 'Software Engineer in San Francisco, CA',
+	longDescription: `After 13 years in the tech industry, I've seen it all - the ups, the
+	downs, and everything in between. I started as a marketer, understanding
+	what people want, what they need, and how to tell them we have it. Now, I
+	build it. I live in San Francisco. I've got a family – a wife and two
+	kids. When I'm not coding, we play tennis or travel. I read a lot, mostly
+	through audiobooks. I'm interested in how we organize our knowledge, how
+	AI is changing our world, and I love good gadgets. Professionally, I code
+	in JavaScript/TypeScript and Python. React, Flask, Express, PostgreSQL,
+	and Node.js are my tools. I use them to build things that make sense.
+	Things that are needed. Things that work. Thanks for visiting. Have a look
+	around, reach out if something catches your eye. Let's build something
+	great together.`,
+};
 
 /**
  * Root layout component
  */
 export default function RootLayout({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-	return (
-		<html
-			lang='en'
-			className={clsx(
-				'text-slate-800 bg-slate-100 dark:text-slate-200 dark:bg-slate-900',
-				jetBrainsMono.variable
-			)}>
-			<body className={jetBrainsMono.variable}>
-			<div className='Home flex flex-col items-center justify-between w-full h-screen'>
-			<SiteNav />
-			<div className='Home-body flex flex-col items-center p-10'>
-				{children}
-				</div>
-			<Footer />
-		</div>
-			</body>
-		</html>
-	);
+    return (
+        <html
+            lang='en'
+            className={clsx(
+                'text-slate-800 bg-slate-100 dark:text-slate-200 dark:bg-slate-900',
+                inter.className
+            )}>
+            <body className={inter.className}>
+                <div className='Home flex flex-row items-center justify-between w-full h-screen'>
+                    <div className={`w-1/3 bg-slate-200 dark:bg-slate-800 h-full pt-10 pl-5 md:fixed top-0`}>
+                        <NewSiteNav />
+                    </div>
+                    <div className='Home-body w-2/3 ml-auto h-full flex flex-col items-center p-10'>
+                        {children}
+                    </div>
+                    {/* <Footer /> */}
+                </div>
+            </body>
+        </html>
+    );
 }
