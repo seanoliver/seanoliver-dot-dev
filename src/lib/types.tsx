723b30007ba3746803895ae550eac9fb6dc614eb
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-13 23:30:28 -0700

    moved nav types to types file

--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -6,6 +6,26 @@ export type Project = {
     tags: string[];
 }
 
+/**
+ * This type defines props for the main menu. It includes passed-in state and a
+ * state setter, as well as the menu content.
+ */
+export type MenuProps = {
+	isOpen: boolean;
+	setIsOpen: (isOpen: boolean) => void;
+	children: React.ReactNode;
+};
+
+/**
+ * This type defines props for the nav item.
+ */
+export type NavItemProps = {
+	item: {
+		name: string;
+		url: string;
+	};
+};
+
 export interface CardWrapperProps {
     children: React.ReactNode;
 }
