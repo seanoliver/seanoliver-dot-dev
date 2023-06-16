bbe8d67889db802b53dc73f12b56fdae88c39f38
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-15 22:59:36 -0700

    experimenting with animations in site nav

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -10,19 +11,30 @@ import { NAV_ITEMS } from '@/lib/globals';
 
 export default function SiteNav(): JSX.Element {
 	const [isOpen, setIsOpen] = useState(false);
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+		// const timeout = setTimeout(() => setIsMounted(true), 0);
+		// return () => clearTimeout(timeout);
+	}, []);
 
 	return (
 		<NavWrapper>
-			<Nametag />
+			<Nametag isMounted={isMounted} />
 			<ResponsiveMenu
 				isOpen={isOpen}
 				setIsOpen={setIsOpen}>
-				{NAV_ITEMS.map((item, index) => (
-					<NavItem
-						key={`${item.name}-${index}`}
-						item={item}
-					/>
-				))}
+				<AnimatePresence>
+					{NAV_ITEMS.map((item, index) => (
+						<NavItem
+							key={`${item.name}-${index}`}
+							item={item}
+							index={index}
+							isMounted={isMounted}
+						/>
+					))}
+				</AnimatePresence>
 			</ResponsiveMenu>
 		</NavWrapper>
 	);
@@ -41,10 +53,30 @@ export default function SiteNav(): JSX.Element {
  */
 const NavWrapper = ({ children }: CardWrapperProps) => {
 	return (
-		<div className='flex flex-row items-center justify-around'>
-			{children}
-		</div>
+		<div className='flex flex-row items-center justify-around'>{children}</div>
 	);
+};
+
+const variationLeft = {
+	hidden: {
+		opacity: 0,
+		x: -100,
+	},
+	visible: {
+		opacity: 1,
+		x: 0,
+	},
+};
+
+const variationRight = {
+	hidden: {
+		opacity: 0,
+		x: 100,
+	},
+	visible: {
+		opacity: 1,
+		x: 0,
+	},
 };
 
 /**
@@ -54,27 +86,35 @@ const NavWrapper = ({ children }: CardWrapperProps) => {
  * @example
  * <Nametag />
  */
-const Nametag = () => {
+const Nametag = ({ isMounted }: { isMounted: boolean }) => {
 	return (
-		<div className='relative rounded-xl overflow-auto p-8 hidden md:block'>
-			<div className='overflow-visible relative max-w-sm mx-auto bg-white shadow-lg ring-1 ring-black/5 rounded-xl flex items-center gap-6 dark:bg-slate-700 md:w-80 dark:highlight-white/5'>
-				<Image
-					className={`absolute -left-6 w-24 h-24 rounded-full shadow-lg`}
-					src='/../public/profile.jpeg'
-					width={96}
-					height={96}
-					alt='Sean Oliver'
-				/>
-				<div className='flex flex-col py-5 pl-24'>
-					<strong className='text-slate-900 text-sm font-medium dark:text-slate-200'>
-						Sean Oliver
-					</strong>
-					<span className='text-slate-500 text-sm font-medium dark:text-slate-400'>
-						Software Engineer
-					</span>
+		<AnimatePresence>
+			<motion.div
+				variants={variationLeft}
+				initial='hidden'
+				animate={isMounted ? 'visible' : 'hidden'}
+				transition={{ delay: 0.15, type: 'spring' }}
+				exit='hidden'
+				className={`relative rounded-xl overflow-auto p-8 hidden md:block`}>
+				<div className='overflow-visible relative max-w-sm mx-auto bg-white shadow-lg ring-1 ring-black/5 rounded-xl flex items-center gap-6 dark:bg-slate-700 md:w-80 dark:highlight-white/5'>
+					<Image
+						className={`absolute -left-6 w-24 h-24 rounded-full shadow-lg`}
+						src='/../public/profile.jpeg'
+						width={96}
+						height={96}
+						alt='Sean Oliver'
+					/>
+					<div className='flex flex-col py-5 pl-24'>
+						<strong className='text-slate-900 text-sm font-medium dark:text-slate-200'>
+							Sean Oliver
+						</strong>
+						<span className='text-slate-500 text-sm font-medium dark:text-slate-400'>
+							Software Engineer
+						</span>
+					</div>
 				</div>
-			</div>
-		</div>
+			</motion.div>
+		</AnimatePresence>
 	);
 };
 
@@ -120,7 +160,7 @@ const ResponsiveMenu: React.FC<MenuProps> = ({
 			<nav
 				className={`
 			md:flex md:justify-end md:flex-col rounded-md m-6 md:m-0
-			top-full left-0 bg-slate-200 dark:bg-slate-800
+			top-full left-0 bg-sate-200 dark:bg-slate-800
 			md:static md:bg-transparent ${isOpen ? 'block' : 'hidden'}`}>
 				{children}
 			</nav>
@@ -136,29 +176,23 @@ const ResponsiveMenu: React.FC<MenuProps> = ({
  * @example
  * <NavItem item={{ name: 'Home', url: '/' }} />
  */
-const NavItem: React.FC<NavItemProps> = ({ item }) => {
+const NavItem: React.FC<NavItemProps> = ({ item, isMounted, index }) => {
 	return (
-		<div
-		className={`
+		<motion.div
+			variants={variationRight}
+			initial='hidden'
+			animate={isMounted ? 'visible' : 'hidden'}
+			transition={{ delay: index * 0.1, duration: 1, type: 'spring' }}
+			exit='hidden'
+			className={`
 				SiteNav-item
 				p-2
 			`}>
-		<Link
-			className={`
-					transition duration-500 ease-in-out
-					rounded-lg px-3 py-2
-					text-slate-600
-					dark:text-slate-300
-					hover:bg-slate-200
-					dark:hover:bg-slate-700
-					hover:text-slate-900
-					dark:hover:text-slate-100
-					hover:shadow-lg
-					text-center
-				`}
-			href={`${item.url}`}>
-			{item.name}
-		</Link>
-	</div>
-	)
+			<Link
+				className={`transition duration-500 ease-in-out rounded-lg px-3 py-2 text-slate-600 dark:text-slate-300 hover:bg-slate-200 dark:hover:bg-slate-700 hover:text-slate-900 dark:hover:text-slate-100 hover:shadow-lg text-center`}
+				href={`${item.url}`}>
+				{item.name}
+			</Link>
+		</motion.div>
+	);
 };
