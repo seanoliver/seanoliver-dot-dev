024194233be70e0528ec76ae563a4c1be365b001
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-15 22:57:56 -0700

    temporarily rendering sitenav in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Projects from './projects/page';
 import About from './about/page';
 import Experiences from './experiences/page';
+import SiteNav from '../components/navigation';
 
 export default function Home(): JSX.Element {
 	return (
-		<div className='Home mb-5 flex flex-col gap-8'>
-			<About />
-			<Projects />
-			<Experiences />
+		<div className='Home flex h-screen'>
+			<SiteNav />
+			{/* <div className='snap-section'><About /></div>
+			<div className='snap-section'><Projects /></div>
+			<div className='snap-section'><Experiences /></div> */}
 		</div>
 	);
 }
