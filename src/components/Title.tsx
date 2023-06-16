aa106c9efc7f6fc5f5b83d5da25c777e455d8e48
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-15 22:58:29 -0700

    title animations

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+const titleVariants = {
+	hidden: { opacity: 0, y: -100 },
+	visible: { opacity: 1, y: 0 },
+};
+
 /**
  *
  * @param title Title text to display
@@ -23,7 +30,11 @@ export default function Title({
 	}
 	return (
 		<>
-			<h1
+			<motion.h1
+				variants={titleVariants}
+				initial='hidden'
+				animate='visible'
+				transition={{ duration: 0.75 }}
 				className={`
 				${titleStyles[titleSize]} text-center p-2 mb-4
 				bg-clip-text text-transparent
@@ -31,7 +42,7 @@ export default function Title({
 				dark:bg-gradient-to-r dark:from-stone-300 dark:to-slate-300 dark:text-white
 				px-2`}>
 				{title}
-			</h1>
+			</motion.h1>
 		</>
 	);
 }
