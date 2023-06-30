fc13fb18fbd1f26c6647f8a5b60bd3b43ecbf2cf
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-30 16:13:34 -0700

    correct yoe

--- src/app/experiences/page.tsx
+++ src/app/experiences/page.tsx
@@ -1,18 +1,30 @@
-import Title from "@/components/Title";
-import { Card, CardDescription, CardSubtitle, CardTitle } from "@/components/cards";
-import { EXPERIENCES } from "@/lib/globals";
+import Title from '@/components/Title';
+import {
+	Card,
+	CardDescription,
+	CardSubtitle,
+	CardTitle,
+} from '@/components/cards';
+import { EXPERIENCES } from '@/lib/globals';
 
 export default function Experiences(): JSX.Element {
-    return (
-        <div className='Experiences flex flex-col items-center justify-center w-full'>
-            <Title title='Work Experience' titleSize='medium' />
-            {EXPERIENCES.map((experience, index) => (
-                <Card key={index}>
-                    <CardTitle>{experience.company}</CardTitle>
-                    <CardSubtitle>{experience.title}</CardSubtitle>
-                    <CardDescription>{experience.description}</CardDescription>
-                </Card>
-            ))}
-        </div>
-    )
+	return (
+		<section
+			id='experiences'
+			className='mb-80'>
+			<div className='Experiences flex flex-col items-center justify-center w-full'>
+				<Title
+					title='Work Experience'
+					titleSize='medium'
+				/>
+				{EXPERIENCES.map((experience, index) => (
+					<Card key={index}>
+						<CardTitle>{experience.company}</CardTitle>
+						<CardSubtitle years={experience.date}>{experience.title}</CardSubtitle>
+						<CardDescription>{experience.description}</CardDescription>
+					</Card>
+				))}
+			</div>
+		</section>
+	);
 }
