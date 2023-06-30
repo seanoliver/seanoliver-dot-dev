93d9ee34cb7c5d64447e6a2a9713a45a38ac91b5
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-30 16:13:04 -0700

    wip about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-
 import Title from '@/components/Title';
+import { CardTag, CardTags } from '@/components/cards';
 import { SOCIAL_LINKS } from '@/lib/globals';
 
 /** About me metadata */
@@ -22,28 +20,49 @@ const bodyVariants = {
 	visible: { opacity: 1, y: 0 },
 };
 
+const myStack = [
+	'React',
+	'Next.js',
+	'Node.js',
+	'Typescript',
+	'Javascript',
+	'SQL',
+	'Python',
+	'Flask',
+	'HTML',
+	'CSS',
+	'Sass',
+	'PostgreSQL',
+	'AWS S3',
+	'Electron',
+	'Git',
+	'GitHub',
+];
+
 /**
  * About me page
  * @returns About me page
  * @example
  * <About />
- * @see https://seanoliver.dev/about
  */
 export default function About(): JSX.Element {
 	return (
-		<section id='about'>
-			<div className='About flex flex-col mx-auto'>
-				<Title title={metadata.title} />
-				<div className='flex'>
-					{/* <PhotoLockup /> */}
-					<motion.p
-						variants={bodyVariants}
-						initial='hidden'
-						animate='visible'
-						transition={{ duration: 0.75 }}
-						className={`mt-4`}>
+		<section id='about' className='mb-80'>
+			<div className='About'>
+				<Title title='About Me' />
+				<div className='flex justify-center gap-8'>
+					<div className={`mt-4 w-1/4 p-5 rounded-lg`}>
+						<Title title='Get to know me!' titleSize='small' />
 						{metadata.longDescription}
-					</motion.p>
+					</div>
+					<div className={`mt-4 w-1/4 p-5 rounded-lg`}>
+						<Title title='My Stack' titleSize='small' />
+						<CardTags>
+							{myStack.map((tag, index) => (
+								<CardTag key={index}>{tag}</CardTag>
+							))}
+						</CardTags>
+					</div>
 				</div>
 				<SocialLinks />
 			</div>
@@ -51,56 +70,18 @@ export default function About(): JSX.Element {
 	);
 }
 
-const variants = {
-	hidden: { opacity: 0, y: 100 },
-	visible: { opacity: 1, y: 0 },
-};
-
-const PhotoLockup = () => {
-	return (
-		<motion.div
-			className={`relative rounded-xl overflow-auto p-8 md:block`}
-			style={{ minWidth: 'max-content' }}>
-			<div className='overflow-visible relative max-w-sm mx-auto pt-5 bg-white shadow-lg ring-1 ring-black/5 rounded-xl flex flex-col items-center dark:bg-slate-700 dark:highlight-white/5'>
-				<ProfilePic />
-				<div className='flex flex-col p-5 text-center'>
-					<strong className='text-slate-900 text-sm font-medium dark:text-slate-200'>
-						Hi, I&apos;m Sean
-					</strong>
-				</div>
-			</div>
-		</motion.div>
-	);
-};
-
-const ProfilePic = () => {
-	return (
-		<Image
-			className={`flex -left-6 w-24 h-24 rounded-full shadow-lg`}
-			src='/../public/profile.jpeg'
-			width={96}
-			height={96}
-			alt='Sean Oliver'
-		/>
-	);
-};
-
 const SocialLinks = (): JSX.Element => {
 	return (
 		<div className='About-social-icons flex flex-row justify-center mt-10'>
 			{SOCIAL_LINKS.map((link, index) => (
-				<motion.a
-					variants={variants}
-					initial='hidden'
-					animate='visible'
-					transition={{ delay: 0.1 + index * 0.1 }}
+				<a
 					key={`${link.name}-${index}`}
 					className={`About-social-link text-2xl m-5`}
 					href={link.url}
 					target='_blank'
 					rel='noopener noreferrer'>
 					{link.icon}
-				</motion.a>
+				</a>
 			))}
 		</div>
 	);
