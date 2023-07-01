f7193ce5c0ef4b9d1f8b4924d21a0e8d9df43f5a
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-30 21:56:59 -0700

    config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-// TODO: Remove placehold.co from remotePatterns once profile image is available
-const nextConfig = {
-  images: {
-    dangerouslyAllowSVG: true,
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: 'placehold.co'
-    },
-    {
-      protocol: 'https',
-      hostname: 'images.unsplash.com'
-    }]
-  }
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
