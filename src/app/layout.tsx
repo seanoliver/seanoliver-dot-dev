4b568d8faaded43a5e0fd9c929de5796e5ce53a9
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-30 21:57:17 -0700

    streamline + dark mode toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,7 @@
 import './globals.css';
 import clsx from 'clsx';
 import Nav from '@/components/navigation';
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import { variationUp } from '@/components/AnimationVariants';
+import { ThemeProvider } from '@/components/theme-provider';
 
 /** Inter Font */
 
@@ -24,39 +22,21 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	const [isMounted, setIsMounted] = useState(false);
-
-	useEffect(() => {
-		setIsMounted(true);
-	}, []);
 	return (
 		<html
 			lang='en'
-			className={clsx(
-				'text-slate-800 bg-slate-300 dark:text-slate-200 dark:bg-slate-700',
-				'bg-gradient-to-b bg-no-repeat',
-				'dark:from-slate-900 dark:to-slate-700',
-				'from-slate-100 to-slate-300',
-				inter.className
-			)}>
+			className={clsx('text-slate-800 bg-slate-300 dark:text-slate-200 dark:bg-slate-700 bg-gradient-to-b bg-no-repeat dark:from-slate-900 dark:to-slate-700 from-slate-100 to-slate-300', inter.className)}>
 			<body className={`${inter.className} flex flex-col min-h-screen`}>
-				<Nav />
-				<div className='flex-1 overflow-hidden'>
-					{isMounted ? (
-						<motion.div
-							variants={variationUp}
-							initial='hidden'
-							animate={isMounted ? 'visible' : 'hidden'}
-							transition={{ delay: 0.25, type: 'spring' }}
-							exit={{ opacity: 0 }}
-							className='overflow-y-auto'>
-							{children}
-						</motion.div>
-					) : (
-						<div style={{ visibility: 'hidden' }}>{children}</div>
-					)}
-					<Footer />
-				</div>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem>
+					<div className='flex flex-column justify-center items-center'>
+						<Nav className='xl:max-w-xl lg:max-w-lg md:max-w-md sm:max-w-sm xs:max-w-xs mx-auto flex flex-col mt-5' />
+						{children}
+						<Footer />
+					</div>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
