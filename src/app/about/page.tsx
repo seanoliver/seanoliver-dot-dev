9e4476b174f7e59a7bf4834a9f235376703fddb1
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-07-13 11:30:37 -0700

    🛠️refactor(page) Remove unused code ⚙️chore(dependencies) Update globals import 🛠️refactor(About) Comment out unused variables 🚀feat(About) Add custom link component ⚙️chore(dependencies) Comment out SocialLinks component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,42 +1,42 @@
 'use client';
 
-import { SOCIAL_LINKS } from '@/lib/globals';
+// import { SOCIAL_LINKS } from '@/lib/globals';
 import Link from 'next/link';
 
 /** About me metadata */
-const metadata = {
-	title: 'Sean Oliver',
-	description: 'Software Engineer in San Francisco, CA',
-	longDescription: `I'm a software engineer and recovering
-	product marketer with 13 years of experience building and growing
-	technology products. My background in marketing helps me understand
-	people's needs and how to communicate with them. Now as a software
-	engineer, I build products that meet those needs.`,
-};
+// const metadata = {
+// 	title: 'Sean Oliver',
+// 	description: 'Software Engineer in San Francisco, CA',
+// 	longDescription: `I'm a software engineer and recovering
+// 	product marketer with 13 years of experience building and growing
+// 	technology products. My background in marketing helps me understand
+// 	people's needs and how to communicate with them. Now as a software
+// 	engineer, I build products that meet those needs.`,
+// };
 
-const bodyVariants = {
-	hidden: { opacity: 0, y: 50 },
-	visible: { opacity: 1, y: 0 },
-};
+// const bodyVariants = {
+// 	hidden: { opacity: 0, y: 50 },
+// 	visible: { opacity: 1, y: 0 },
+// };
 
-const myStack = [
-	'React',
-	'Next.js',
-	'Node.js',
-	'Typescript',
-	'Javascript',
-	'SQL',
-	'Python',
-	'Flask',
-	'HTML',
-	'CSS',
-	'Sass',
-	'PostgreSQL',
-	'AWS S3',
-	'Electron',
-	'Git',
-	'GitHub',
-];
+// const myStack = [
+// 	'React',
+// 	'Next.js',
+// 	'Node.js',
+// 	'Typescript',
+// 	'Javascript',
+// 	'SQL',
+// 	'Python',
+// 	'Flask',
+// 	'HTML',
+// 	'CSS',
+// 	'Sass',
+// 	'PostgreSQL',
+// 	'AWS S3',
+// 	'Electron',
+// 	'Git',
+// 	'GitHub',
+// ];
 
 /**
  * Custom link component for use on about page
@@ -143,19 +143,19 @@ export default function About(): JSX.Element {
 	// );
 }
 
-const SocialLinks = (): JSX.Element => {
-	return (
-		<div className='About-social-icons flex flex-row justify-center mt-10'>
-			{SOCIAL_LINKS.map((link, index) => (
-				<a
-					key={`${link.name}-${index}`}
-					className={`About-social-link text-2xl m-5`}
-					href={link.url}
-					target='_blank'
-					rel='noopener noreferrer'>
-					{link.icon}
-				</a>
-			))}
-		</div>
-	);
-};
+// const SocialLinks = (): JSX.Element => {
+// 	return (
+// 		<div className='About-social-icons flex flex-row justify-center mt-10'>
+// 			{SOCIAL_LINKS.map((link, index) => (
+// 				<a
+// 					key={`${link.name}-${index}`}
+// 					className={`About-social-link text-2xl m-5`}
+// 					href={link.url}
+// 					target='_blank'
+// 					rel='noopener noreferrer'>
+// 					{link.icon}
+// 				</a>
+// 			))}
+// 		</div>
+// 	);
+// };
