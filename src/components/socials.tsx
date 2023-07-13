486078999829393de5ae042b146ad5027d9f1332
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-07-13 11:25:56 -0700

    🛠️refactor(socials) fix formatting and style in socials component

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,12 +1,17 @@
-import { SOCIAL_LINKS } from "@/lib/globals"
-import ExternalLink from "./external-link"
+import { SOCIAL_LINKS } from '@/lib/globals';
+import ExternalLink from './external-link';
 
 export default function Socials(): JSX.Element {
-    return (
-        <>
-            {SOCIAL_LINKS.map((link, index) => (
-                <ExternalLink key={index} className="px-2" href={link.url}>{link.name}</ExternalLink>
-            ))}
-        </>
-    )
+	return (
+		<>
+			{SOCIAL_LINKS.map((link, index) => (
+				<ExternalLink
+					key={index}
+					className='px-2'
+					href={link.url}>
+					{link.name}
+				</ExternalLink>
+			))}
+		</>
+	);
 }
