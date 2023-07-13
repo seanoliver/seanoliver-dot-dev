f9162be9a923d25c7061424a4359c36e40da3c50
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-07-13 00:41:01 -0700

    add goodreads

---
+++ src/components/goodreads.tsx
@@ -0,0 +1,65 @@
+import GoodreadsBookshelf from 'react-goodreads-shelf';
+
+export default function Goodreads(): JSX.Element {
+	return (
+		<>
+			<div className=''>
+				<p className='leading-7'>
+					I love reading, but not with my eyes. I&apos;ve been addicted to
+					audiobooks since 2019 on a trip to China when I gulped down{' '}
+					<i>Bad Blood</i>, <i>Educated</i>, and a few other totally kickass
+					books. I aim to &quot;read&quot; about 30 books per year, and usually
+					hit it.
+				</p>
+				<br />
+				<p>Here are the 30 I&apos;ve most recently read:</p>
+				<br />
+				<GoodreadsBookshelf
+					userId='26616336'
+					limit={30}
+					width={50}
+                    displayOptions={{
+                        hideBackgroundImages: undefined,
+                        hideDetails: {
+                            author: true,
+                            rating: true,
+                            subtitle: true,
+                            title: true,
+                        },
+                    }}
+				/>
+			</div>
+		</>
+	);
+}
+
+/**
+ *
+    GoodreadsBookshelf Schema: https://kylekarpack.github.io/react-goodreads-shelf/?path=/docs/react-goodreads-shelf--documentation
+
+<GoodreadsBookshelf
+    displayOptions={{
+        hideBackgroundImages: undefined,
+        hideDetails: {
+            author: true,
+            rating: true,
+            subtitle: true,
+            title: true
+        }
+    }}
+    hideDetails={[
+        'title',
+        'subtitle',
+        'author',
+        'rating'
+    ]}
+    limit={20}
+    order="d"
+    search=""
+    shelf="read"
+    sort="date_read"
+    userId="63515611"
+    width={100}
+/>
+
+ */
