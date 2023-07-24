53d60ab82475d288decd9e5537245b8df7654ac8
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-07-23 22:50:26 -0700

    feat(next.config) add contentlayer, feat(next.config) enable react strict mode and swc minify

--- next.config.js
+++ next.config.js
@@ -1,17 +1,23 @@
+const { withContentlayer } = require('next-contentlayer');
+
 /** @type {import('next').NextConfig} */
 // TODO: Remove placehold.co from remotePatterns once profile image is available
 const nextConfig = {
-  images: {
-    dangerouslyAllowSVG: true,
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: 'placehold.co'
-    },
-    {
-      protocol: 'https',
-      hostname: 'images.unsplash.com'
-    }]
-  }
-}
+	images: {
+		dangerouslyAllowSVG: true,
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'placehold.co',
+			},
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com',
+			},
+		],
+	},
+	reactStrictMode: true,
+	swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig)
