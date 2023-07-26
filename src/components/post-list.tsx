b186abc857cbc3abd32559401567c6e0771bcc27
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-07-25 22:59:51 -0700

    feat(post-list) add new PostList component

---
+++ src/components/post-list.tsx
@@ -0,0 +1,28 @@
+import { format, parseISO } from 'date-fns';
+import { UnderLink } from '@/components/under-link';
+import { Post } from 'contentlayer/generated';
+
+export default function PostList({ posts, limit }: { posts: Post[], limit?: number}) {
+
+	if (limit) {
+		posts = posts.slice(0, limit);
+	}
+
+	return (
+		<div className='mx-auto max-w-xl'>
+			{posts.map((post, idx) => (
+				<div key={idx} className='w-full flex items-center justify-between leading-7'>
+					<span className='font-medium'>
+						<UnderLink href={post.url}>
+							{post.title}
+						</UnderLink>
+					</span>
+					<span className='text-muted-foreground text-xs'>
+						{format(parseISO(post.date), 'MMMM dd, yyyy')}
+					</span>
+				</div>
+
+			))}
+			</div>
+	);
+}
