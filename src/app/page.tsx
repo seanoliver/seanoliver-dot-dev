66fc5515a35654c45e9d8342d1c28cb1c943b29a
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-08-12 13:25:34 -0700

    reordered imports in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 'use client'
 
-import Goodreads from '@/components/goodreads'
-import Section from '@/components/Section'
-import Socials from '@/components/socials'
-import About from './about/page'
-import Experience from './experience/page'
-import Projects from './projects/page'
-import Posts from './posts/page'
+import Goodreads from '@/components/goodreads';
+import Socials from '@/components/socials';
+import About from './about/page';
+import Experience from './experience/page';
+import Posts from './posts/page';
+import Projects from './projects/page';
 
 export default function Home(): JSX.Element {
   return (
