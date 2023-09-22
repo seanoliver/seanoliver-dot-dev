6cd89d0f2d4b050a3e06b0b8b4eb757cb2d0e5e9
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-09-21 17:15:43 -0700

    integrated seo optimizations with sitemap and robots files

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+        userAgent: '*',
+        allow: ['/', '/api/og'],
+        disallow: '/private/',
+      },
+    sitemap: 'https://seanoliver.dev/sitemap.xml',
+    host: 'https://seanoliver.dev',
+  }
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from 'next'
+import { allPosts } from 'contentlayer/generated'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const routes = ['', '/posts', '/experience', '/projects', '/about'].map((route) => ({
+    url: `https://seanoliver.dev${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  const posts =
+    allPosts
+      .filter((post) => post.isPublished)
+      .map((post) => ({
+        url: `https://seanoliver.dev/posts/${post.slug}`,
+        lastModified: post.date,
+      })) ?? []
+
+  return [...routes, ...posts]
+}
