66ab9711eb614d41bae321bdfdb882793b57b8d3
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-11-13 08:47:23 -0800

    update bio and og next package

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Section from '@/components/Section'
 import { UnderLink } from '@/components/under-link'
-import { Metadata } from 'next';
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'About',
@@ -30,9 +30,10 @@ export default function About(): JSX.Element {
     <Section title="About">
       <div className="About">
         <p className="leading-7 [&:not(:first-child)]:mt-6">
-          I’m a software engineer and former product marketer who loves building and growing
-          technology products that delight users. I have 13 years of experience at leading companies
-          like <UnderLink href="https://www.microsoft.com/">Microsoft</UnderLink>,{' '}
+          I’m an AI engineer who loves building and growing products that leverage AI that solve
+          important problems in surprising and delightful ways. I have 13 years of experience
+          building and growing products at leading companies like{' '}
+          <UnderLink href="https://www.microsoft.com/">Microsoft</UnderLink>,{' '}
           <UnderLink href="https://www.linkedin.com/">LinkedIn</UnderLink>,{' '}
           <UnderLink href="https://www.lyft.com/">Lyft</UnderLink>, and{' '}
           <UnderLink href="https://www.block.xyz/">Block</UnderLink>.
@@ -43,21 +44,13 @@ export default function About(): JSX.Element {
           natural and effortless.
         </p>
         <p className="leading-7 [&:not(:first-child)]:mt-6">
-          In 2023, I traded my full-time job in marketing for a seat at{' '}
-          <UnderLink href="https://rithmschool.com">Rithm School</UnderLink>, a 16-week software
-          engineering bootcamp that taught me how to program—not how to code. In addition to{' '}
-          <UnderLink href="https://www.typescriptlang.org/">TypeScript</UnderLink> and{' '}
-          <UnderLink href="https://www.python.org/">Python</UnderLink>, it taught me how to create
-          strategically and how to evaluate technical tradeoffs.
-        </p>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
           I have a <UnderLink href="https://newsletter.seanoliver.dev/">newsletter</UnderLink> where
           I write about programming, productivity, personal knowledge management.
         </p>
         <p className="leading-7 [&:not(:first-child)]:mt-6">
           I live in San Francisco, CA with my amazing wife, two children, and our 3.5 lbs dog named
           Pixel (no relation to the phone). I love productivity tools and systems, personal
-          knowledge management, artificial intelligence, tennis, and casual mobile games.
+          knowledge management, AI, tennis, and casual mobile games.
         </p>
       </div>
     </Section>
